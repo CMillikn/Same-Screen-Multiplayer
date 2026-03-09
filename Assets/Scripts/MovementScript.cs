a0806b1f7c4fe68553a3bc07914e1e024e08b8e9
Conrad Milliken <conrad.milliken@gmail.com>
2026-03-08 22:17:24 -0700

    yeag

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -11,6 +11,7 @@ public class MovementScript : MonoBehaviour
     [SerializeField] List<GameObject> watcherObjects = new List<GameObject>();
     [SerializeField] float playerSpeed;
     [SerializeField] float playerAccel;
+    private bool isSlapped;
 
     void Start()
     {
@@ -21,34 +22,50 @@ public class MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_thisRB.linearVelocity.magnitude > playerSpeed)
+        if (!isSlapped)
         {
-            _thisRB.linearVelocity = Vector3.ClampMagnitude(_thisRB.linearVelocity, playerSpeed);
-        }
+            if (_thisRB.linearVelocity.magnitude > playerSpeed)
+            {
+                _thisRB.linearVelocity = Vector3.ClampMagnitude(_thisRB.linearVelocity, playerSpeed);
+            }
+    
+            if (_thisKB.wKey.isPressed)
+            {
+                _thisRB.linearVelocity = new Vector3(_thisRB.linearVelocity.x, 0, (_thisRB.linearVelocity.z + playerAccel * Time.deltaTime));
+            }
+            
+            if (_thisKB.sKey.isPressed)
+            {
+                _thisRB.linearVelocity = new Vector3(_thisRB.linearVelocity.x, 0, (_thisRB.linearVelocity.z - playerAccel * Time.deltaTime));
+            }
+    
+            if (_thisKB.aKey.isPressed)
+            {
+                _thisRB.linearVelocity = new Vector3((_thisRB.linearVelocity.x - playerAccel * Time.deltaTime), 0, _thisRB.linearVelocity.z);
+            }
+    
+            if (_thisKB.dKey.isPressed)
+            {
+                _thisRB.linearVelocity = new Vector3((_thisRB.linearVelocity.x + playerAccel * Time.deltaTime), 0, _thisRB.linearVelocity.z);
+            }
 
-        if (_thisKB.wKey.isPressed)
-        {
-            _thisRB.linearVelocity = new Vector3(_thisRB.linearVelocity.x, 0, (_thisRB.linearVelocity.z + playerAccel * Time.deltaTime));
+            if (_thisKB.spaceKey.isPressed)
+            {
+                
+            }
         }
         
-        if (_thisKB.sKey.isPressed)
-        {
-            _thisRB.linearVelocity = new Vector3(_thisRB.linearVelocity.x, 0, (_thisRB.linearVelocity.z - playerAccel * Time.deltaTime));
-        }
+    }
 
-        if (_thisKB.aKey.isPressed)
-        {
-            _thisRB.linearVelocity = new Vector3((_thisRB.linearVelocity.x - playerAccel * Time.deltaTime), 0, _thisRB.linearVelocity.z);
-        }
+    public void GetSlapped()
+    {
+        StartCoroutine(Slap());
+    }
 
-        if (_thisKB.dKey.isPressed)
-        {
-            _thisRB.linearVelocity = new Vector3((_thisRB.linearVelocity.x + playerAccel * Time.deltaTime), 0, _thisRB.linearVelocity.z);
-        }
-
-        watcherObjects[0].transform.position = new Vector3(this.transform.position.x, 0.5f, 4.5f);
-        watcherObjects[1].transform.position = new Vector3(this.transform.position.x, 0.5f, -4.5f);
-        watcherObjects[2].transform.position = new Vector3(-4.5f, 0.5f, this.transform.position.z);
-        watcherObjects[3].transform.position = new Vector3(4.5f, 0.5f, this.transform.position.z);
+    IEnumerator Slap()
+    {
+        isSlapped = true;
+        yield return new WaitForSeconds(1);
+        isSlapped = false;
     }
 }
